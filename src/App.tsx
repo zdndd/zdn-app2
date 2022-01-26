7c51edbf724552bcf1b32bd2f34df358af26c05b
Dongning Zhang <dongningzhang@knx.com.cn>
2022-01-26 18:43:20 +0800

    可以从5-2开始了

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { ProjectListScreen } from './screens/project-list'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { ProjectListScreen } from "./screens/project-list";
+import { LoginScreen } from "./screens/login";
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen></ProjectListScreen>
+      {/* <ProjectListScreen></ProjectListScreen>
+       */}
+
+      <LoginScreen></LoginScreen>
     </div>
   );
 }
