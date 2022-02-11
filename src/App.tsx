41ee104bf61472c48c1df604984271fd91771e8f
Dongning Zhang <dongningzhang@knx.com.cn>
2022-02-11 11:49:20 +0800

    7-5 error boundary 也没有太整明白

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,16 @@ import { AuthenticatedApp } from "./authenticated-app";
 import { UnauthenticatedApp } from "./unauthenticated-app";
 
 import { useAuth } from "./context/auth-context";
+import { ErrorBoundary } from "./components/error-boundary";
+import { FullPageErrorFallback } from "./components/lib";
 
 function App() {
   const { user } = useAuth();
   return (
     <div className="App">
-      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      <ErrorBoundary fallbackRender={FullPageErrorFallback}>
+        {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      </ErrorBoundary>
     </div>
   );
 }
